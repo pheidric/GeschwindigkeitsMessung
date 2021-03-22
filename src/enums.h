1cb1da57a3fd93b2d70bde501744f927be9d6ea4
Peter Heidrich <peter-heidrich@web.de>
2021-03-22 20:36:18 +0100

    Alten Code übernommen

---
+++ src/enums.h
@@ -0,0 +1,22 @@
+//#ifndef STATE
+//enum STATE {
+//	S_IDLE,
+//	S_RUNNING,
+//	S_DONE
+//};
+//#endif
+//
+//#ifndef DISPLAY_MODE
+//enum DISPLAY_MODE {
+//	DM_TIME,
+//	DM_SPEED,
+//	DM_SPEED_SCALED
+//};
+//#endif
+//
+//#ifndef UNIT
+//enum UNIT {
+//	U_MS,
+//	U_KMH
+//};
+//#endif
---
+++ src/konfiguration.ino
@@ -0,0 +1,39 @@
+///**
+// * Konfiguration für Geschwindigkeitsmessung
+// * v1.0
+// * 15.02.2020 10:47
+// * Arduino Uno
+// */
+//
+//
+//// Konfiguration
+//
+//const int DISTANCE = 14;			// cm
+//const int SCALE = 87;
+//const int DEBOUNCE_SENSOR = 5000;	// ms
+//const int DEBOUNCE_BUTTONS = 500;	// ms
+//
+//const byte DISPLAY_BRIGHTNESS = 7;	// 0 .. 7
+//
+//// Pins
+//
+//const int PIN_SENSOR_1 = 2;
+//const int PIN_SENSOR_2 = 3;
+//
+//const int PIN_BTN_RESET = 4;
+//const int PIN_BTN_DISPLAY = 5;
+//const int PIN_BTN_UNIT = 6;
+//
+//const int PIN_LED_STATE_IDLE = 7;
+//const int PIN_LED_STATE_RUNNING = 8;
+//const int PIN_LED_STATE_DONE = 9;
+//
+//const int PIN_LED_MODE_TIME = 10;
+//const int PIN_LED_MODE_SPEED = 11;
+//const int PIN_LED_MODE_SPEED_SCALED = 12;
+//
+//const int PIN_LED_UNIT_MS = 13;
+//const int PIN_LED_UNIT_KMH = 14;	// A0
+//
+//const int PIN_DISPLAY_DIO = 15;		// A1
+//const int PIN_DISPLAY_CLK = 16;		// A2
